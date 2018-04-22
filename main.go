82401c6e7fa4bee3918f12bb6ef94ab4d5b17af7
sirrah23 <hseth93@gmail.com>
2018-04-22 19:38:33 -0400

    Initial web app that can serve HTML files

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",
+			t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr string = ":8080"
+	http.Handle("/", &templateHandler{filename: "index.html"})
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
